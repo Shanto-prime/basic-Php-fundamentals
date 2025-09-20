32abcad1cda5e860243a01a273103f99ad527342
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-20 13:28:35 +0600

    refactor index.php to improve color styling and foreach implementation

--- index.php
+++ index.php
@@ -13,21 +13,29 @@
                 padding-inline-start: 20px;
             }
         }
+        .o2red{
+            color: red;
+        }
+        .o2green{
+            color: green;
+        }
+        .o2blue{
+            color: blue;
+        }
+        .o2skyblue{
+            color: skyblue;
+        }
     </style>
 </head>
 
 <body>
     <h4>Test Foreach function</h4>
-    <?php
-    $colors =["red", "green", "blue", "skyblue"];
-    ?>
+    <?php $colors =["red", "green", "blue", "skyblue"]; ?>
     <div class="box">
         <ul class="list">
-            <?php
-            foreach($colors as $color){
-                echo "<li style='color:$color'>$color</li>";
-            }
-            ?>
+            <?php foreach($colors as $color): ?>
+                <li class="o2<?= $color; ?>"><?php echo "the color is" ." " . $color; ?></li>
+            <?php endforeach; ?>
         </ul>
     </div>
 </body>
