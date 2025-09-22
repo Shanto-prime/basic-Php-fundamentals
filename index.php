5ab31cc7b092e10bfdd211f64a08c9acebe5f81d
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-22 19:00:35 +0600

    Refactor routing logic: move routing functionality from index.php to a new router.php file for better organization and maintainability.

--- index.php
+++ index.php
@@ -2,22 +2,5 @@
 
 require_once 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-    '/' => 'controller/index.php',
-    '/about' => 'controller/about.php',
-    '/contact' => 'controller/contact.php'
-];
-
-
-
-
-
-if (array_key_exists($uri, $routes)) {
-    require_once $routes[$uri];
-} else {
-    http_response_code(404);
-    require_once 'views/404.php';
-    die();
-}
+require 'router.php';
