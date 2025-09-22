0241ac61673c1776052877c047fc9fba526f0f39
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-22 22:19:37 +0600

    Remove commented-out router inclusion and clean up unused code in index.php

--- index.php
+++ index.php
@@ -3,4 +3,37 @@
 require_once 'functions.php';
 
 
-require 'router.php';
+// require 'router.php';
+
+// connect to database
+
+$dsn = "mysql:host=localhost;port=3306; dbname=demo-php;";
+$pdo = new PDO($dsn , 'root', '11110000');
+$statement = $pdo->prepare("SELECT * FROM demos");
+$statement->execute();
+$demos = $statement->fetchAll(pdo::FETCH_ASSOC);
+dd($demos);
+
+// class person
+// {
+//     public $name;
+//     public $age;
+//     public $email;
+//     public $password;
+//     public $id;
+
+//     public function breathe()
+//     {
+//         echo $this->name . " is breathing";
+//     }
+// }
+
+// $person = new person();
+// $person->name = "Ymha";
+// $person->age = 22;
+// $person->email = "y@y.com";
+// $person->password = "123456";
+// $person->id = 1;
+
+// $person -> breathe();
+?>
