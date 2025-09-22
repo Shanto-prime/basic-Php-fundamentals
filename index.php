8e80a382518e15419e92c6a6e7d792b8e159ed3d
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-22 16:59:31 +0600

    Refactor URL handling and add custom 404 page: update urlIs function to use parse_url and implement routing logic in index.php; create a new 404 view with Tailwind CSS styling.

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once 'functions.php';
 
-if (urlIs('/')) {
-    require_once 'controller/index.php';
-} elseif (urlIs('/about')) {
-    require_once 'controller/about.php';
-} elseif (urlIs('/contact')) {
-    require_once 'controller/contact.php';
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controller/index.php',
+    '/about' => 'controller/about.php',
+    '/contact' => 'controller/contact.php'
+];
+
+
+
+
+
+if (array_key_exists($uri, $routes)) {
+    require_once $routes[$uri];
+} else {
+    http_response_code(404);
+    require_once 'views/404.php';
+    die();
 }
