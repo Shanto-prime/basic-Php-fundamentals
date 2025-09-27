22eca835a7085fb772ae9d761e07c96b9d7fb293
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-27 23:03:20 +0600

    Remove commented-out code and debugging statements in index.php for cleaner codebase.

--- index.php
+++ index.php
@@ -12,32 +12,9 @@ $pdo = new PDO($dsn , 'root', '11110000');
 $statement = $pdo->prepare("SELECT * FROM demos");
 $statement->execute();
 $demos = $statement->fetchAll(pdo::FETCH_ASSOC);
-// dd($demos);
 
 foreach ($demos as $demo) {
     echo $demo['author'] . "<br>";
 }
 
-// class person
-// {
-//     public $name;
-//     public $age;
-//     public $email;
-//     public $password;
-//     public $id;
-
-//     public function breathe()
-//     {
-//         echo $this->name . " is breathing";
-//     }
-// }
-
-// $person = new person();
-// $person->name = "Ymha";
-// $person->age = 22;
-// $person->email = "y@y.com";
-// $person->password = "123456";
-// $person->id = 1;
-
-// $person -> breathe();
 ?>
