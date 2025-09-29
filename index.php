122c69399af85a716a66dcccfa642ddac4652bed
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-09-29 14:51:53 +0600

    Refactor database query logic: encapsulate database connection and query execution within the Database class in index.php for improved structure and readability.

--- index.php
+++ index.php
@@ -5,16 +5,26 @@ require_once 'functions.php';
 
 // require 'router.php';
 
-// connect to database
-
-$dsn = "mysql:host=localhost;port=3306; dbname=demo-php;";
-$pdo = new PDO($dsn , 'root', '11110000');
-$statement = $pdo->prepare("SELECT * FROM demos");
-$statement->execute();
-$demos = $statement->fetchAll(pdo::FETCH_ASSOC);
-
-foreach ($demos as $demo) {
-    echo $demo['author'] . "<br>";
+class Database
+{
+    public function query($query = "SELECT * FROM demos")
+    {
+        // connect to database
+        echo "Connected to database" . "<br>";
+        $dsn = "mysql:host=localhost;port=3306; dbname=demo-php;";
+        $pdo = new PDO($dsn, 'root', '11110000');
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll(pdo::FETCH_ASSOC);
+    }
 }
 
-?>
+$db = new Database();
+
+$demos = $db->query();
+
+
+
+foreach ($demos as $demo) {
+    echo $demo['title'] . " by " . $demo['author'] . "<br>";
+}
