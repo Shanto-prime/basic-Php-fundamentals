6b330905e316578c7124e4dd3bc9a8f80befe551
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-10-27 12:26:27 +0600

    Update demo query in index.php to filter by ID for specific demo retrieval

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
 require_once 'functions.php';
 
+require 'views/Database.php';
 
 // require 'router.php';
 
 
 $db = new Database();
 
-$demos = $db->query("SELECT * FROM demos ")->fetch(pdo::FETCH_ASSOC);
+$demos = $db->query("SELECT * FROM demos where id =5 ")->fetch(pdo::FETCH_ASSOC);
 
 
 dd($demos["title"]);
