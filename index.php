e23196b78220fe721e1375ff060082a787e4b5b2
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-10-28 22:06:56 +0600

    Refactor Database class constructor to use configuration array for improved maintainability and clarity; update index.php to fetch all demo records.

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ require 'views/Database.php';
 
 $db = new Database();
 
-$demos = $db->query("SELECT * FROM demos where id =5 ")->fetch(pdo::FETCH_ASSOC);
+$demos = $db->query("SELECT * FROM demos where id =5 ")->fetchAll();
 
 
-dd($demos["title"]);
+// dd($demos["title"]);
 
 // foreach ($demos as $demo) {
 //     echo $demo['title'] . " by " . $demo['author'] . "<br>";
--- views/Database.php
+++ views/Database.php
@@ -6,10 +6,21 @@ class Database
 
     public function __construct()
     {
-        echo "Connected to database" . "<br>";
-        $dsn = "mysql:host=localhost;port=3306; dbname=demo-php;";
-        $this -> connection = new PDO($dsn, 'root', '11110000');
+        $config =[
+            'host' => 'localhost',
+            'port' => '3306',
+            'dbname' => 'demo-php'
+        ];
+
+        http_build_query($config);
+
+        echo "Connected to Internet" . "<br>";
+        $dsn = "mysql:host={$config['host']};port={$config['port']}; dbname={$config['dbname']};";
+        $this -> connection = new PDO($dsn, 'root', '11110000',[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
+    
     public function query($query = "SELECT * FROM demos")
     {
         // connect to database
